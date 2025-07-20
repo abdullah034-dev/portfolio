d6ff66d398a835febb9634073c6f193825417e9f
abdullah034-dev <191107@students.au.edu.pk>
2025-07-20 13:03:51 +0500

    Update readme

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,7 +36,7 @@ function ContactForm() {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `/api/contact`,
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
         userInput
       );
 
